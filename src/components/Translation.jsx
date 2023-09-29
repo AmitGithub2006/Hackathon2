9fba5154509c5e88e75d8030c09d1f94d6df675d
AmitGithub2006 <105305969+AmitGithub2006@users.noreply.github.com>
2023-09-29 19:29:24 +0530

    translation added

--- src/components/Translation.jsx
+++ src/components/Translation.jsx
@@ -1,13 +1,104 @@
-import React from 'react'
-import Box from '@mui/material/Box';
-function Translation() {
+import React, { useState } from "react";
+import "./Translation.css";
+import OpenAI from "openai";
+import { BeatLoader } from "react-spinners";
+
+const Translation = () => {
+  const [formData, setFormData] = useState({ language: "Hindi", message: "" });
+  const [error, setError] = useState("");
+  const [showNotification, setShowNotification] = useState(false);
+  const [translation, setTranslation] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const apiKey = process.env.REACT_APP_API_KEY;
+
+  const openai = new OpenAI({ apiKey: apiKey, dangerouslyAllowBrowser: true });
+
+  const handleInputChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+    setError("");
+  };
+
+  const translate = async () => {
+    const { language, message } = formData;
+    const response = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "system",
+          content:
+            "You will be provided with a sentence in English, and your task is to translate it into Hindi.",
+        },
+        {
+          role: "user",
+          content: `${formData.message}`,
+        },
+      ],
+      temperature: 0,
+      max_tokens: 256,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+    });
+
+    const translatedText = response.choices[0].message.content;
+    setIsLoading(false);
+    setTranslation(translatedText);
+  };
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    if (!formData.message) {
+      setError("Please enter the message.");
+      return;
+    }
+    setIsLoading(true);
+    translate();
+  };
+
+  const handleCopy = () => {
+    navigator.clipboard
+      .writeText(translation)
+      .then(() => displayNotification())
+      .catch((err) => console.error("failed to copy: ", err));
+  };
+
+  const displayNotification = () => {
+    setShowNotification(true);
+    setTimeout(() => {
+      setShowNotification(false);
+    }, 3000);
+  };
+
   return (
-    <Box  sx ={{p:10}}>
-        <h1>Translation</h1>
+    <div className="main">
+      <h1>Translation</h1>
 
-    </Box>
-    
-  )
-}
+      <form onSubmit={handleOnSubmit}>
+        <div className="choices">
+          <h4>Translate to Hindi</h4>
+        </div>
 
-export default Translation
+        <textarea
+          name="message"
+          placeholder="Type your message here.."
+          onChange={handleInputChange}
+        ></textarea>
+
+        {error && <div className="error">{error}</div>}
+
+        <button type="submit">Translate</button>
+      </form>
+
+      <div className="translation">
+        {isLoading ? <BeatLoader size={12} color={"red"} /> : translation}
+      </div>
+
+      <div className={`notification ${showNotification ? "active" : ""}`}>
+        Copied to clipboard!
+      </div>
+    </div>
+  );
+};
+
+export default Translation;
